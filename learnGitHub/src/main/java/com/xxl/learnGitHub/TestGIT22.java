86075b7ee61dbbf410b6e22903eac766d60ad555
xiexiaoliang <xiexl.jason@gmail.com>
2016-12-02 08:45:19 +0800

    222222

---
+++ learnGitHub/src/main/java/com/xxl/learnGitHub/TestGIT22.java
@@ -0,0 +1,20 @@
+/**
+ * <p>Title: TestGIT22.java</p>
+ * <p>Description:learnGitHub </p>
+ * <p>Copyright:2016年12月2日 </p>
+ * <p>Company: rongshu</p>
+ * <p>author: xie.xiaoliang</p>
+ * <p>package: com.xxl.learnGitHub</p>	
+ * @version v1.0.0
+ */
+package com.xxl.learnGitHub;
+
+/**
+ * Description:
+ * @author: xie.xiaoliang
+ * @CreateDate: 2016年12月2日
+ * @version: V1.0
+ */
+public class TestGIT22 {
+
+}
